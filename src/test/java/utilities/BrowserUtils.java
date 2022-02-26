cbde250be87e86c3c84287d9e55a9a10adb744db
ruveyda <ruveyda.crr@gmail.com>
2022-02-26 08:32:02 -0700

    CRM project is created

---
+++ src/test/java/utilities/BrowserUtils.java
@@ -0,0 +1,67 @@
+package utilities;
+/*
+In this class only general utility methods that are not related to some specific page.
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
+public class BrowserUtils {
+    /*
+    This method will accept integer(in seconds) and execute Thread.sleep for given duration
+     */
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+        for (String each : allWindowsHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+
+
+        }
+        //5. Assert:Title contains “expectedInTitle”
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+        /*
+        This method accepts a String "expectedTitle" and Asserts if it is true
+         */
+        public static void verifyTitle(WebDriver driver, String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+
+
+    }
+    }
+
+
+
+
+/*
+TC #2: Create utility method
+ 1. Create a new class called BrowserUtils
+2. Create a method to make Task1 logic re-usable
+3. When method is called, it should switch window and verify title.
+
+
+Method info:
+• Name: switchWindowAndVerify
+• Return type: void
+• Arg1: WebDriver
+• Arg2: String expectedInUrl
+• Arg3: String expectedTitle
+ */
